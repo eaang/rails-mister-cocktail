8926275e805a0838a912cfd9ce8dc74c3d693f36
Evan Ang <evan.a.ang@gmail.com>
2020-06-25 12:27:10 +0800

    made routes

--- .history/config/routes_20200625122607.rb
+++ .history/config/routes_20200625122607.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create]
   end
-  resources :doses, only: [:destroy]
 end
