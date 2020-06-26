cc0d743f2e9f6084b7e860317f833759937cfc44
Evan Ang <evan.a.ang@gmail.com>
2020-06-26 19:56:29 +0800

    did up a full height header

---
+++ .history/app/javascript/components/scroll_arrows_20200626191607.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let em;
+
+  function getValue(id){
+    const div = document.querySelector(id);
+    div.style.height = '1em';
+    return ( em = div.offsetHeight );
+  }
+
+  function fixBox(id){
+    const div = document.querySelector(id);
+    div.style.height = '0em';
+  }
+
+  getValue("#sizing-box")
+
+  const down_arrow = document.querySelector("#down")
+  down_arrow.addEventListener("click", () => {
+    window.scrollBy({ 
+      top: window.innerHeight - (7 * em),
+      left: 0, 
+      behavior: 'smooth' 
+    })
+  })
+
+  const up_arrow = document.querySelector("#up")
+  up_arrow.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    })
+  })
+})
---
+++ .history/app/javascript/components/scroll_arrows_20200626195527.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let em;
+
+  function getValue(id){
+    const div = document.querySelector(id);
+    div.style.height = '1em';
+    return ( em = div.offsetHeight );
+  }
+
+  getValue("#sizing-box")
+
+  const down_arrow = document.querySelector("#down")
+  down_arrow.addEventListener("click", () => {
+    window.scrollBy({ 
+      top: window.innerHeight - (7 * em),
+      left: 0, 
+      behavior: 'smooth' 
+    })
+  })
+
+  const up_arrow = document.querySelector("#up")
+  up_arrow.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    })
+  })
+})
